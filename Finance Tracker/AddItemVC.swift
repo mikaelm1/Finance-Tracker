d303e28a6764a18cb5809fc741d9e0f491aa37ad
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-07-03 14:31:05 -0700

    feat: Added segmented control for choosing either income or expense

--- Finance Tracker/AddItemVC.swift
+++ Finance Tracker/AddItemVC.swift
@@ -9,11 +9,55 @@
 import UIKit
 
 class AddItemVC: UIViewController {
+    
+    lazy var transactionTypeSegment: UISegmentedControl = {
+        let items = ["Income", "Expense"]
+        let s = UISegmentedControl(items: items)
+        s.translatesAutoresizingMaskIntoConstraints = false
+        s.tintColor = UIColor.greenColor()
+        s.addTarget(self, action: #selector(transactionTypeChanged), forControlEvents: .ValueChanged)
+        s.selectedSegmentIndex = 0
+        return s
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.blueColor()
+        view.backgroundColor = UIColor.whiteColor()
+        setupViews()
+    }
+    
+    func setupViews() {
+        view.addSubview(transactionTypeSegment)
+        
+        view.addConstraintsWithFormat("V:|-80-[v0(35)]", views: transactionTypeSegment)
+        transactionTypeSegment.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor, constant: 30).active = true
+        transactionTypeSegment.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor, constant: -30).active = true
+        
+    }
+    
+    func transactionTypeChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            sender.tintColor = UIColor.greenColor()
+        case 1:
+            sender.tintColor = UIColor.redColor()
+        default:
+            break
+        }
     }
 
+}
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerate() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
 }
