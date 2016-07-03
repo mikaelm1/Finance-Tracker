cdd5636eed6e264c7789493198f51ef86f19fc3a
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-07-03 13:36:17 -0700

    feat: Added two vc for stats and entering income/expense

--- Finance Tracker/CustomTabBarController.swift
+++ Finance Tracker/CustomTabBarController.swift
@@ -13,10 +13,18 @@ class CustomTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let statsController = UIViewController()
-        statsController.view.backgroundColor = UIColor.redColor()
+        let statsController = createController(StatsVC(), title: "Stats", imageName: "stats")
+        let addItemsController = createController(AddItemVC(), title: "New", imageName: "income_expense")
         
-        viewControllers = [statsController]
+        viewControllers = [statsController, addItemsController]
+    }
+    
+    private func createController(viewController: UIViewController, title: String, imageName: String) -> UINavigationController {
+        let vc = viewController
+        let navVC = UINavigationController(rootViewController: vc)
+        navVC.tabBarItem.title = title
+        navVC.tabBarItem.image = UIImage(named: imageName)
+        return navVC
     }
 
 }
---
+++ Finance Tracker/StatsVC.swift
@@ -0,0 +1,19 @@
+//
+//  StatsVC.swift
+//  Finance Tracker
+//
+//  Created by Mikael Mukhsikaroyan on 7/3/16.
+//  Copyright © 2016 MSquaredmm. All rights reserved.
+//
+
+import UIKit
+
+class StatsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.redColor()
+    }
+
+}
