e3dcae29bbc58b9a90f73d85af8dfca9c86a7b80
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-07-03 14:32:39 -0700

    refactor: Moved view extensio to seperate file

---
+++ Finance Tracker/ViewExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  ViewExtensions.swift
+//  Finance Tracker
+//
+//  Created by Mikael Mukhsikaroyan on 7/3/16.
+//  Copyright © 2016 MSquaredmm. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerate() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
