5d34a1f4dac5f4b63ef7330f2c641e8b39e6e9a2
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-07-04 12:41:05 -0700

    feat: Added type field to model

--- Finance Tracker/Transaction.swift
+++ Finance Tracker/Transaction.swift
@@ -15,15 +15,17 @@ class Transaction: Object {
     dynamic var name = ""
     dynamic var price: Double = 0.0
     dynamic var created = NSDate()
+    dynamic var type = ""
     
     override class func primaryKey() -> String? {
         return "transactionId"
     }
     
-    convenience init(name: String, price: Double) {
+    convenience init(name: String, price: Double, type: String) {
         self.init()
         self.name = name
         self.price = price
+        self.type = type 
     }
     
 }
