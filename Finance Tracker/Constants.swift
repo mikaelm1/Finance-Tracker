58f991651e7a9c5e3ee0e828b5e720371bdfa0ed
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-07-05 14:38:15 -0700

    feat: Changed nav and tab bar appearances

--- Finance Tracker/Constants.swift
+++ Finance Tracker/Constants.swift
@@ -12,6 +12,7 @@ struct Constants {
     
     static let expenseColor = UIColor.rgb(244, green: 67, blue: 54, alpha: 1)
     static let incomeColor = UIColor.rgb(0, green: 230, blue: 118, alpha: 1)
+    static let purpleBarColor = UIColor.rgb(156, green: 39, blue: 176, alpha: 1)
     
     static let priceFieldTag = 10
     static let nameFieldTag = 20
--- Finance Tracker/CustomTabBarController.swift
+++ Finance Tracker/CustomTabBarController.swift
@@ -13,6 +13,9 @@ class CustomTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tabBar.barTintColor = Constants.purpleBarColor
+        tabBar.tintColor = UIColor.whiteColor()
+        
         let statsController = createController(StatsVC(), title: "Stats", imageName: "stats")
         let addItemsController = createController(AddItemVC(), title: "New", imageName: "income_expense")
         
@@ -22,6 +25,9 @@ class CustomTabBarController: UITabBarController {
     private func createController(viewController: UIViewController, title: String, imageName: String) -> UINavigationController {
         let vc = viewController
         let navVC = UINavigationController(rootViewController: vc)
+        navVC.navigationBar.barTintColor = Constants.purpleBarColor
+        navVC.navigationBar.tintColor = UIColor.whiteColor()
+        navVC.navigationBar.barStyle = .Black
         navVC.tabBarItem.title = title
         navVC.tabBarItem.image = UIImage(named: imageName)
         return navVC
